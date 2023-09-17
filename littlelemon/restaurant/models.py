529fc91d59ba0aac50fcdc442333cb45977c0cd3
Brandon Weaver <bmweaver92@gmail.com>
2023-09-17 23:12:43 +0200

    Added user authentication

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -9,10 +9,10 @@ class Booking(models.Model):
     def __str__(self): 
         return self.name
     
-class Menu(models.Model):
-   title = models.CharField(max_length=255) 
-   price = models.DecimalField(max_digits=10, decimal_places=2) 
-   inventory = models.IntegerField()
-   
-   def __str__(self):
-      return self.title
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.SmallIntegerField()
+    
+    def get_item(self):
+        return f'{self.title} : {str(self.price)}'
--- littlelemon/restaurant/serializers.py
+++ littlelemon/restaurant/serializers.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from .models import Menu, Booking
+from .models import MenuItem, Booking
 
-class MenuSerializer(serializers.ModelSerializer):
+class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Menu
-        fields = ['title', 'price', 'inventory']
+        model = MenuItem
+        fields = '__all__'
 
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
